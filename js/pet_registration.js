2246a677bce468e49ac2578872be5c990b8829c0
AlexanderReeves <reevesdrums@gmail.com>
2024-08-16 15:16:45 +1000

    Animations and rego form loader

---
+++ js/pet_registration.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('#petquiz').submit(function(e) {
+      //Prevent page reload attempt
+      e.preventDefault();
+	  
+
+      //Remove old warnings
+      $(".errors").remove();
+      var validSelection = true;
+
+
+    });
+
+
+});
+
--- js/pet_test.js
+++ js/pet_test.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     $('#petquiz').submit(function(e) {
       //Prevent page reload attempt
       e.preventDefault();
+	  
 
       //Remove old warnings
       $(".errors").remove();
@@ -32,25 +33,37 @@ $(document).ready(function() {
       var resultTextColour = "lowScore";
       //This code only runs if no errors were thrown
 	  if(validSelection){
+		//Show the registration button after 5 seconds
+		setTimeout(createRegButton, 5000 );
+
 	    if(petScore >=0 && petScore <=10){
 	  		$("#resultFish").removeClass("hidden");
 	  		var $fadeInImage = $('#resultFish'); 
 		    $fadeInImage.fadeTo(5000, 1.0, function(){
 				$fadeInImage.addClass('active');
-			}); 
+			});
 	  	}
 
 	  	if(petScore >=11 && petScore <=25){
 	  		$("#resultCat").removeClass("hidden");
 	  		var resultTextColour = "medScore";
+			var $stretchImage = $('#resultCat'); 
+			$stretchImage.animate({width: '200px'}, 5000);			
 	  	}
 	  	if(petScore >=26 && petScore <=50){
 	  		$("#resultDog").removeClass("hidden");
 	  		var resultTextColour = "highScore";
+			var $slideImage = $('#resultDog'); 
+			//This one is quite interesting...
+			//It increases the entire page width, however as it returns to its
+			//normal location, the page resumes the normal size and
+			//I can't see any true negatives to this approach apart from being quircky which
+			//I quite like!
+			$slideImage.animate({'padding-left': '0px'}, 5000);	
 	  	}
 
-	  	$("#resultDog").after('<h2 class="centertext ' + resultTextColour + '" >Your score is ' + petScore +'</h2>');
-	  	$("#endQuiz").after('<span class="centertext" >Scroll down to see your results!</span>');
+	  	$("#resultDog").after('<h2 id="displayScore" class="centertext  ' + resultTextColour + '" >Your score is ' + petScore +'</h2>');
+	  	$("#endQuiz").after('<span class="centertext"  id="scrollMessage" >Scroll down to see your results!</span>');
 	  	//Remove the submit button
 	  	$("#submitTest").remove();
 	  }
@@ -58,6 +71,11 @@ $(document).ready(function() {
 
 
 });
+
+function createRegButton(){
+	$("#displayScore").after(
+		'<form action="./a3Registration.html"> <input type="submit" value="Adopt Me!" class="button center"/> </form>');
+}
 
 
 
